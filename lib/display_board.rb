8107ce76db039ed5fc2c6ec443269953da94e3e5
Timothy Miller <mr.tim.miller123@gmail.com>
2016-04-14 17:47:49 -0600

    Changed display_board.rb to create the tick tac toe board

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,14 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+
+def display_board
+  cell = "   "
+  devider = "|"
+  row = "-----------"
+  puts cell + devider + cell + devider + cell
+  puts row
+  puts cell + devider + cell + devider + cell
+  puts row
+  puts cell + devider + cell + devider + cell
+end
+
+display_board
